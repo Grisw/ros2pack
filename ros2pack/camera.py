f2f276eb8e9c514aba8fe3eb06e091c05c7ffa55
grisw <514335188@qq.com>
2020-12-22 17:12:23 +0800

    update camera, lidar.

---
+++ ros2pack/camera.py
@@ -0,0 +1,36 @@
+import roslibpy
+import cv2
+import numpy as np
+import time
+
+
+class Camera:
+
+    def __init__(self, host='localhost', port=9090):
+        self.ros = roslibpy.Ros(host=host, port=port)
+        self.ros.run()
+        self.listener = roslibpy.Topic(self.ros, '/camera', 'sensor_msgs/msg/CompressedImage')
+        self.listener.subscribe(self._on_msg)
+        self.image = None
+        while self.image is None:
+            time.sleep(0.1)
+
+    def _on_msg(self, msg):
+        buf = np.ndarray(shape=(1, len(msg['data'])),
+                         dtype=np.uint8, buffer=bytes(msg['data']))
+        self.image = cv2.imdecode(buf, cv2.IMREAD_ANYCOLOR)
+
+    def save(self, name):
+        assert self.image is not None, 'No image received.'
+        cv2.imwrite(name, self.image)
+
+    def show(self, winname='camera'):
+        assert self.image is not None, 'No image received.'
+        while True:
+            cv2.imshow(winname, self.image)
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+
+    def __del__(self):
+        self.ros.terminate()
--- ros2pack/lidar.py
+++ ros2pack/lidar.py
@@ -1,4 +1,7 @@
 import roslibpy
+import numpy as np
+import cv2
+import math
 
 
 class Lidar:
@@ -12,6 +15,24 @@ class Lidar:
 
     def _on_msg(self, msg):
         self.distances = msg['ranges']
+
+    def show(self):
+        center = (320, 320)
+        point_size = 1
+        point_color = (0, 0, 255)  # BGR
+        thickness = 1
+        while True:
+            img = np.zeros((640, 640, 3), np.uint8)
+            cv2.circle(img, center, point_size, (0, 255, 0), 3)
+            for i, d in enumerate(self.distances):
+                if d is None or np.isinf(d) or np.isnan(d):
+                    continue
+                p_x = d * 50 * -math.cos(i * (2 * math.pi) / 360)
+                p_y = d * 50 * math.sin(i * (2 * math.pi) / 360)
+                cv2.circle(img, (int(center[0] + p_x), int(center[1] + p_y)), point_size, point_color, thickness)
+                cv2.imshow('image', img)
+            if cv2.waitKey(1) == 27:
+                break
 
     def __del__(self):
         self.ros.terminate()
