c8dd7f00c01752bf57f36210260c013850ad6363
grisw <514335188@qq.com>
2020-12-29 15:07:59 +0800

    fix imu bug.

--- ros2pack/imu.py
+++ ros2pack/imu.py
@@ -21,7 +21,7 @@ class IMU:
     def quaternion_to_euler(self, w, x, y, z):
         """w、x、y、z to euler angles"""
         r = math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
-        p = math.asin(2 * (w * y - z * z))
+        p = math.asin(2 * (w * y - z * x))
         y = math.atan2(2 * (w * z + x * y), 1 - 2 * (z * z + y * y))
 
         roll = r * 180 / math.pi
